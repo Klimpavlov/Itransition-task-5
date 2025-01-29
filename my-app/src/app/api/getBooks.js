2f3a16165cdda30d8d6ab88cf7c7221fc416c67f
Klimpavlov <Klimpavlov2002@gmail.com>
2025-01-29 22:11:13 +0300

    check deploy

--- my-app/src/app/api/getBooks.js
+++ my-app/src/app/api/getBooks.js
@@ -2,7 +2,9 @@ import axios from "axios";
 
 export async function getBooksList() {
     try {
-       const response =  await axios.get("http://localhost:3001/books");
+        const url = "https://itransition-task-5-be99.onrender.com/books";
+        // const url = "http://localhost:3001/books";
+       const response =  await axios.get(url);
        console.log(response);
        return response.data;
     }
